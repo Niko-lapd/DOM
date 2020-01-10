04ac1040fc5ac3ba3914b2feab7b167525e2a6a4
Niko-lapd <54631340+Niko-lapd@users.noreply.github.com>
2020-01-10 08:53:44 +0100

    Initial commit

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById("pagetitle").innerText = "Dit is de pagina titel";
+document.getElementById("subheader").innerText = "Dit is de subkop";
+document.getElementById("aap1").src = "img/aap1.png";
+document.getElementById("aap2").src = "img/aap2.png";
